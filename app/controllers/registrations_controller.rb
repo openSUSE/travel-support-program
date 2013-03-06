3cec2cd7680214393b4b69f74970fa1586c85ef5
Ancor Gonzalez Sosa <ancor@suse.de>
2013-03-06 18:51:36 +0100

    Registrations controller for account update

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,22 @@
+class RegistrationsController < Devise::RegistrationsController
+  def update
+    # required for settings form to submit when password is left blank
+    if params[:user][:password].blank?
+      params[:user].delete("password")
+      params[:user].delete("password_confirmation")
+    end
+
+    @user = User.find(current_user.id)
+    if @user.update_attributes(params[:user])
+      # Sign in the user bypassing validation in case his password changed
+      #set_flash_key :notice, :updated
+      flash[:notice] = 'Dreamland'
+      sign_in @user, :bypass => true
+      set_flash_message :notice, :updated
+      redirect_to edit_user_registration_path
+    else
+      set_flash_message :alert, :update_failed
+      render "edit"
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 TravelSupportProgram::Application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => {:registrations => 'registrations'}
 
   resources :requests do
     resources :submissions, :controller => :request_submissions
