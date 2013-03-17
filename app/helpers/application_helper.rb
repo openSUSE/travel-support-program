f2d1255f7b6de0cc8fb9f38c4ccfdc517af14aee
Ancor Gonzalez Sosa <ancor@suse.de>
2013-03-17 13:11:34 +0100

    Bugfix and documentation in ApplicationHelper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,25 @@
 module ApplicationHelper
+  # Outputs a <li> element suitable for use in Bootstrap nav menus, that is,
+  # with the 'active' css class when needed
+  #
+  # param [String] label Text to display
+  # param [String] path  Target URL
+  # return [String] HTML output
   def nav_tab(label, path)
     css = current_page?(path) ? "active" : nil
     content_tag(:li, link_to(t(label), path), :class => css)
   end
 
+  # Outputs country code in a human readable and localized way, using the locale
+  # file for localized_country_select
+  #
+  # param [String] country_code The ISO
+  # return [String] HTML output
   def country_label(country_code)
-    country_code + " - " + I18n.t("countries.#{country_code}")
+    if country_code.nil?
+      t("show_for.blank")
+    else
+      country_code + " - " + t("countries.#{country_code}")
+    end
   end
 end
