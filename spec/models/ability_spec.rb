a7d30e871f3b65bbf9bc118cfae7f5df2361b044
Ancor Gonzalez Sosa <ancor@suse.de>
2013-04-01 19:19:32 +0200

    Fixed typo in specs

--- spec/models/ability_spec.rb
+++ spec/models/ability_spec.rb
@@ -12,43 +12,43 @@ describe Ability do
 
     context 'managing events' do
       it{ should be_able_to(:create, Event.new) }
-      it{ should_not be_able_to(:update, events(:javin_hackaton)) }
+      it{ should_not be_able_to(:update, events(:yavin_hackaton)) }
       it{ should be_able_to(:update, events(:party)) }
-      it{ should_not be_able_to(:destroy, events(:javin_hackaton)) }
+      it{ should_not be_able_to(:destroy, events(:yavin_hackaton)) }
       it{ should_not be_able_to(:destroy, events(:party)) }
     end
 
     context 'managing his own requests' do
       it{ should be_able_to(:create, Request.new) }
       it{ should be_able_to(:read, requests(:luke_for_party)) }
-      it{ should be_able_to(:read, requests(:luke_for_javin)) }
+      it{ should be_able_to(:read, requests(:luke_for_yavin)) }
       it{ should be_able_to(:update, requests(:luke_for_party)) }
-      it{ should_not be_able_to(:update, requests(:luke_for_javin)) }
+      it{ should_not be_able_to(:update, requests(:luke_for_yavin)) }
       it{ should be_able_to(:destroy, requests(:luke_for_party)) }
-      it{ should_not be_able_to(:destroy, requests(:luke_for_javin)) }
+      it{ should_not be_able_to(:destroy, requests(:luke_for_yavin)) }
       it{ should be_able_to(:cancel, requests(:luke_for_party)) }
-      it{ should_not be_able_to(:cancel, requests(:luke_for_javin)) }
+      it{ should_not be_able_to(:cancel, requests(:luke_for_yavin)) }
     end
 
     context "trying to look into other's requests" do
-      it{ should_not be_able_to(:read, requests(:wedge_for_javin)) }
+      it{ should_not be_able_to(:read, requests(:wedge_for_yavin)) }
       it{ should_not be_able_to(:read, requests(:wedge_for_party)) }
       it{ should_not be_able_to(:update, requests(:wedge_for_party)) }
     end
 
     context "asking for a correct reimbursement" do
-      it{ should be_able_to(:create, requests(:luke_for_javin).build_reimbursement) }
+      it{ should be_able_to(:create, requests(:luke_for_yavin).build_reimbursement) }
     end
 
     context "asking for an incorrect reimbursement" do
       let(:user){ users(:wedge) }
-      it{ should_not be_able_to(:create, requests(:luke_for_javin).build_reimbursement) }
-      it{ should_not be_able_to(:create, requests(:wedge_for_javin).build_reimbursement) }
+      it{ should_not be_able_to(:create, requests(:luke_for_yavin).build_reimbursement) }
+      it{ should_not be_able_to(:create, requests(:wedge_for_yavin).build_reimbursement) }
     end
 
     context "managing a new reimbursement" do
       before(:each) do
-        @reimbursement = requests(:luke_for_javin).create_reimbursement
+        @reimbursement = requests(:luke_for_yavin).create_reimbursement
       end
 
       it{ should be_able_to(:submit, @reimbursement) }
@@ -70,30 +70,30 @@ describe Ability do
 
     context 'managing events' do
       it{ should be_able_to(:create, Event.new) }
-      it{ should be_able_to(:update, events(:javin_hackaton)) }
+      it{ should be_able_to(:update, events(:yavin_hackaton)) }
       it{ should be_able_to(:update, events(:party)) }
-      it{ should_not be_able_to(:destroy, events(:javin_hackaton)) }
+      it{ should_not be_able_to(:destroy, events(:yavin_hackaton)) }
       it{ should_not be_able_to(:destroy, events(:party)) }
     end
 
     context 'managing requests' do
       it{ should_not be_able_to(:create, Request.new) }
       it{ should be_able_to(:read, requests(:wedge_for_party)) }
-      it{ should be_able_to(:read, requests(:luke_for_javin)) }
+      it{ should be_able_to(:read, requests(:luke_for_yavin)) }
       it{ should be_able_to(:update, requests(:wedge_for_party)) }
       it{ should be_able_to(:approve, requests(:wedge_for_party)) }
       it{ should be_able_to(:roll_back, requests(:wedge_for_party)) }
-      it{ should_not be_able_to(:update, requests(:luke_for_javin)) }
+      it{ should_not be_able_to(:update, requests(:luke_for_yavin)) }
       it{ should_not be_able_to(:destroy, requests(:luke_for_party)) }
-      it{ should_not be_able_to(:destroy, requests(:luke_for_javin)) }
+      it{ should_not be_able_to(:destroy, requests(:luke_for_yavin)) }
       it{ should be_able_to(:cancel, requests(:luke_for_party)) }
       it{ should be_able_to(:cancel, requests(:wedge_for_party)) }
-      it{ should_not be_able_to(:cancel, requests(:luke_for_javin)) }
+      it{ should_not be_able_to(:cancel, requests(:luke_for_yavin)) }
     end
 
     context "managing reimbursements" do
       before(:each) do
-        @reimbursement = requests(:luke_for_javin).create_reimbursement
+        @reimbursement = requests(:luke_for_yavin).create_reimbursement
       end
 
       it{ should_not be_able_to(:create, Reimbursement.new) }
@@ -153,30 +153,30 @@ describe Ability do
 
     context 'managing events' do
       it{ should be_able_to(:create, Event.new) }
-      it{ should_not be_able_to(:update, events(:javin_hackaton)) }
+      it{ should_not be_able_to(:update, events(:yavin_hackaton)) }
       it{ should be_able_to(:update, events(:party)) }
-      it{ should_not be_able_to(:destroy, events(:javin_hackaton)) }
+      it{ should_not be_able_to(:destroy, events(:yavin_hackaton)) }
       it{ should_not be_able_to(:destroy, events(:party)) }
     end
 
     context 'managing requests' do
       it{ should_not be_able_to(:create, Request.new) }
       it{ should be_able_to(:read, requests(:wedge_for_party)) }
-      it{ should be_able_to(:read, requests(:luke_for_javin)) }
+      it{ should be_able_to(:read, requests(:luke_for_yavin)) }
       it{ should_not be_able_to(:update, requests(:wedge_for_party)) }
       it{ should_not be_able_to(:approve, requests(:wedge_for_party)) }
       it{ should_not be_able_to(:roll_back, requests(:wedge_for_party)) }
-      it{ should_not be_able_to(:update, requests(:luke_for_javin)) }
+      it{ should_not be_able_to(:update, requests(:luke_for_yavin)) }
       it{ should_not be_able_to(:destroy, requests(:luke_for_party)) }
-      it{ should_not be_able_to(:destroy, requests(:luke_for_javin)) }
+      it{ should_not be_able_to(:destroy, requests(:luke_for_yavin)) }
       it{ should_not be_able_to(:cancel, requests(:luke_for_party)) }
       it{ should_not be_able_to(:cancel, requests(:wedge_for_party)) }
-      it{ should_not be_able_to(:cancel, requests(:luke_for_javin)) }
+      it{ should_not be_able_to(:cancel, requests(:luke_for_yavin)) }
     end
 
     context "managing reimbursements" do
       before(:each) do
-        @reimbursement = requests(:luke_for_javin).create_reimbursement
+        @reimbursement = requests(:luke_for_yavin).create_reimbursement
       end
 
       it{ should_not be_able_to(:create, Reimbursement.new) }
