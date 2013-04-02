e91892e114874c868b9223951536848a37d779d9
Ancor Gonzalez Sosa <ancor@suse.de>
2013-04-02 10:18:27 +0200

    403 page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,9 @@ class ApplicationController < ActionController::Base
   load_and_authorize_resource :unless => :devise_controller?
 
   protect_from_forgery
+
+  rescue_from CanCan::AccessDenied do |exception|
+    flash[:alert] = nil
+    render :file => "#{Rails.root}/public/403.html", :status => 403
+  end
 end
