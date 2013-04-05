f1f36e738d8610909fde2343c2389fec0732e3b5
Ancor Gonzalez Sosa <ancor@suse.de>
2013-04-05 11:02:28 +0200

    bootstrap_flash_messages helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -117,4 +117,27 @@ module ApplicationHelper
       send helper
     end
   end
+
+  # Outputs the flash messages in a bootstrap optimized way
+  #
+  # Stolen from https://github.com/seyhunak/twitter-bootstrap-rails
+  def bootstrap_flash_messages
+    flash_messages = []
+    flash.each do |type, message|
+      # Skip empty messages, e.g. for devise messages set to nothing in a locale file.
+      next if message.blank?
+
+      type = :success if type == :notice
+      type = :error if type == :alert
+      next unless [:error, :info, :success, :warning].include?(type)
+
+      Array(message).each do |msg|
+        text = content_tag(:div,
+                           content_tag(:button, raw("&times;"), :class => "close", "data-dismiss" => "alert") +
+                           msg.html_safe, :class => "alert fade in alert-#{type}")
+        flash_messages << text if message
+      end
+    end
+    flash_messages.join("\n").html_safe
+  end
 end
