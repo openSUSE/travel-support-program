8fd6446e0a8fc6dac0f944e4ccd4185d4be1e0be
Ancor Gonzalez Sosa <ancor@suse.de>
2013-05-06 15:18:43 +0200

    Missing in the last commit

--- spec/models/final_note_spec.rb
+++ spec/models/final_note_spec.rb
@@ -1,5 +1,7 @@
 require 'spec_helper'
 
 describe FinalNote do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of :user_id }
+  it { should validate_presence_of :body }
+  it { should validate_presence_of :machine }
 end
