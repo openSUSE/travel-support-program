515a4d8df31892657a3ad45972929a1006842b6c
Ancor Gonzalez Sosa <ancor@suse.de>
2013-06-04 14:51:04 +0200

    Some spec:models polishing

--- spec/models/request_expense_spec.rb
+++ spec/models/request_expense_spec.rb
@@ -1,5 +1,5 @@
 require 'spec_helper'
 
 describe RequestExpense do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of :request }
 end
--- spec/models/user_profile_spec.rb
+++ spec/models/user_profile_spec.rb
@@ -1,5 +1,5 @@
 require 'spec_helper'
 
 describe UserProfile do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of :role_id }
 end
