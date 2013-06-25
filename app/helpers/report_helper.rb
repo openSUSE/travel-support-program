3d6974b41e15dee1736e8e88a4885e6f5c097c1b
Ancor Gonzalez Sosa <ancor@suse.de>
2013-06-25 17:30:19 +0200

    More documentation and some bugfixes for reports

--- app/helpers/report_helper.rb
+++ app/helpers/report_helper.rb
@@ -1,4 +1,5 @@
 module ReportHelper
+  # Outputs a dropdown for filtering the expenses report with a given scope
   def filter_select(name, collection, value_method = :id, text_method = :name)
     value = @filter && @filter[name]
     options = content_tag(:option, t(:expenses_report_all), :value => "")
@@ -6,17 +7,25 @@ module ReportHelper
     select_tag "filter[#{name}]", options
   end
 
+  # Outputs a date picker for filtering the expenses report with a given scope
   def filter_datepicker(name)
      value = @filter && @filter[name]
      data = {"date-autoclose" => true, "date-format" => "yyyy-mm-dd", "date-language" => I18n.locale.to_s}
      text_field_tag "filter[#{name}]", value, :class => "date-without-time dpicker", :data => data
   end
 
+  # Outputs a text input for filtering the expenses report with a given scope
   def filter_text_field(name)
     value = @filter && @filter[name]
     text_field_tag "filter[#{name}]", value
   end
 
+  # Outputs the html representation of the field for a given ExpenseReport
+  # instance.
+  #
+  # @param [#to_sym] field name of the field
+  # @param [ExpenseReport] expense the instance
+  # @return HTML output
   def html_value_for(field, expense)
     if respond_to? :"html_value_for_#{field}"
       send(:"html_value_for_#{field}", expense)
@@ -27,6 +36,12 @@ module ReportHelper
     end
   end
 
+  # Return the value of a field for a given ExpenseReport
+  # instance, in a format useful to the axlsx template.
+  #
+  # @param [#to_sym] field name of the field
+  # @param [ExpenseReport] expense the instance
+  # @return [Object] value for the template
   def xlsx_value_for(field, expense)
     if respond_to? :"xlsx_value_for_#{field}"
       send(:"xlsx_value_for_#{field}", expense)
@@ -37,56 +52,86 @@ module ReportHelper
     end
   end
 
+  # Called from html_value_for and xlsx_value_for
+  # @see #html_value_for
+  # @see #xlsx_value_for
   def value_for_request_state(expense)
     Request.human_state_name expense.value_for(:request_state)
   end
 
+  # Called from html_value_for and xlsx_value_for
+  # @see #html_value_for
+  # @see #xlsx_value_for
   def value_for_reimbursement_state(expense)
     state = expense.value_for(:reimbursement_state)
     state.blank? ? "" : Reimbursement.human_state_name(state)
   end
 
+  # Called from html_value_for
+  # @see #html_value_for
   def html_value_for_event_name(expense)
     link_to expense[:event_name], event_path(expense[:event_id])
   end
 
+  # Called from html_value_for
+  # @see #html_value_for
   def html_value_for_request_id(expense)
     link_to "##{expense[:request_id]}", request_path(expense[:request_id])
   end
 
+  # Called from html_value_for
+  # @see #html_value_for
   def html_value_for_reimbursement_id(expense)
     id = expense[:reimbursement_id]
     id.blank? ? "" : link_to("##{id}", request_reimbursement_path(expense[:request_id]))
   end
 
+  # Called from html_value_for
+  # @see #html_value_for
   def html_value_for_sum_amount(expense)
-    number_to_currency(expense.value_for(:sum_amount), :unit => "")
+    number_to_currency(expense.value_for(:sum_amount) || 0, :unit => "")
   end
 
+  # Called from html_value_for
+  # @see #html_value_for
   def html_value_for_event_country(expense)
     content_tag(:span, expense[:event_country], :title => t("countries.#{expense[:event_country]}"))
   end
 
+  # Called from html_value_for
+  # @see #html_value_for
   def html_value_for_user_country(expense)
     content_tag(:span, expense[:user_country], :title => t("countries.#{expense[:user_country]}"))
   end
 
+  # Called from html_value_for
+  # @see #html_value_for
   def html_value_for_event_start_date(expense)
     l(expense.value_for(:event_start_date))
   end
 
+  # Called from html_value_for
+  # @see #html_value_for
   def html_value_for_event_end_date(expense)
     l(expense.value_for(:event_end_date))
   end
 
+  # Called from xlsx_value_for
+  # @see #xlsx_value_for
   def xlsx_value_for_user_country(expense)
     country_label(expense.value_for(:user_country))
   end
 
+  # Called from xlsx_value_for
+  # @see #xlsx_value_for
   def xlsx_value_for_event_country(expense)
     country_label(expense.value_for(:event_country))
   end
 
+  # Style for the axlsx template for a given field
+  #
+  # @param [#to_sym] field name of the field
+  # @return [Hash] Style definition, nil if no special style is defined
   def xlsx_style_for(field)
     if respond_to? :"xlsx_style_for_#{field}"
       send(:"xlsx_style_for_#{field}")
@@ -95,6 +140,8 @@ module ReportHelper
     end
   end
 
+  # Called from xlsx_style_for
+  # @see #xlsx_style_for
   def xlsx_style_for_sum_amount
     {:num_fmt => 2}
   end
--- app/models/expense_report.rb
+++ app/models/expense_report.rb
@@ -1,3 +1,11 @@
+#
+# This model is used to encapsulate the queries involved in the expenses report
+# using the ActiveRecord query interface. It's a read-only model offering a more
+# convenient way of reading information from the request_expenses table, so
+# it's not intended for creating or updating records. Please, use the RequestExpense
+# model for that purpose.
+# @see RequestExpense
+#
 class ExpenseReport < ActiveRecord::Base
   self.table_name = "request_expenses"
 
@@ -50,6 +58,15 @@ class ExpenseReport < ActiveRecord::Base
           { :field => :event_name, :sql => "events.name" },
           { :field => :subject, :sql => "request_expenses.subject" } ] }
 
+  # Main scope for using the whole model. Takes cares of all the grouping,
+  # conditions and selections needed for a given amount field and a given
+  # grouping option. Best examples of use can be found in reports_controller.
+  #
+  # @param [#to_sym] type Type of expense to summarize: :estimated, :approved,
+  #                       :total or :authorized
+  # @param [#to_sym] g  Grouping option. For a list of all the available
+  #                     options, use ExpenseReport.groups
+  # @see .groups                       
   scope :by, lambda {|type, g|
     currency = RequestExpense.currency_field_for(type.to_sym)
     r = joins(:request => [{:user => :profile}, :event])
@@ -59,60 +76,87 @@ class ExpenseReport < ActiveRecord::Base
     # which is quite inefficient. It would be better to find a way to pass the
     # total_count to kaminari
     r = r.select("DISTINCT sum(#{type}_amount) AS sum_amount, #{currency} AS sum_currency, #{@by[g.to_sym].map{|f| "#{f[:sql]} AS #{f[:field]}"}.join(', ')}")
+    r = r.where("#{type}_amount IS NOT NULL")
     r = r.group("#{currency}, #{@by[g.to_sym].map{|f| f[:sql]}.join(', ')}")
   }
 
+  # Scope for filtering
   scope :event_start_lte, lambda {|date|
     where(["events.start_date <= ?", date])
   }
 
+  # Scope for filtering
   scope :event_start_gte, lambda {|date|
     where(["events.start_date >= ?", date])
   }
 
+  # Scope for filtering
   scope :event_eq, lambda {|event_id|
     where("events.id" => event_id)
   }
 
+  # Scope for filtering
   scope :event_name_contains, lambda {|name|
     where(["lower(events.name) like ?", "%#{name}%"])
   }
 
+  # Scope for filtering
   scope :event_country_code_eq, lambda {|country|
     where("events.country_code" => country)
   }
 
+  # Scope for filtering
   scope :user_name_contains, lambda {|name|
     where(["lower(user_profiles.full_name) like ? or lower(users.nickname) like ?"] + ["%#{name}%"]*2)
   }
 
+  # Scope for filtering
   scope :user_country_code_eq, lambda {|country|
     where("user_profiles.country_code" => country)
   }
 
+  # Scope for filtering
   scope :request_state_eq, lambda {|state|
     where("requests.state" => state.to_s)
   }
 
+  # Scope for filtering
   scope :reimbursement_state_eq, lambda {|state|
     where("reimbursements.state" => state.to_s)
   }
 
+  # Ordered list of field names for a given call to the 'by' scope
+  #
+  # @param [#to_sym] group The grouping option used when invoking the scope
+  # @return [array] The names of the resulting fields (as an array of symbols)
   def self.fields_for(group)
     @by[group.to_sym].select {|f| !f[:hidden]}.map {|i| i[:field]} + [ :sum_amount, :sum_currency ]
   end
 
+  # Available group options for calling the 'by' scope
+  #
+  # @return [array] An array with the available grouping criterias (as symbols)
   def self.groups
     @by.keys
   end
 
+  # Casted value of a given attribute.
+  #
+  # Needed because ActiveRecord always return the attributes as strings when
+  # ActiveRelation#select is used. This method casts the attributes back to its
+  # correct type. Intended to be used on records resulting from a call to the
+  # 'by' scope.
+  #
+  # @param [#to_sym] name attribute name
+  # @return [Object] value of the attribute
   def value_for(name)
     # At this point creating a more generic solution is not worthy, we simply
     # check explicitly for one of the three special cases
     if name.to_sym == :sum_amount
-      BigDecimal.new(sum_amount)
+      BigDecimal.new(sum_amount || "0.0")
     elsif [:event_start_date, :event_end_date].include? name.to_sym
-      Date.parse(send(name))
+      d = send(name)
+      d.blank? ? nil : Date.parse(d)
     else
       send(name)
     end
