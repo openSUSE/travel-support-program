268c50b48822be2398ff3a7dc2e8eacb102d00bb
Ancor Gonzalez Sosa <ancor@suse.de>
2014-05-04 16:16:50 +0200

    Better management of secret

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,5 +4,5 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-TravelSupport::Application.config.secret_token = '814559f18a98c99b92689e04a22283698e25334cb99f0652842ca0eeab0a8b7c7c9af593ac7c8aea451db5f794f68ba2dad5ec9b38587446cf56f3869bc01122'
-TravelSupport::Application.config.secret_key_base = 'd60bee54c6e45a4ddf6f86748433c16518f25a41df253df66044519b75c58d710a7c93d8175602a2f435a9cef61aede4e1dc5c9db1fe3bc3dcf3dcd4b899682c'
+TravelSupport::Application.config.secret_token = TravelSupport::Config.setting(:secret, :token)
+TravelSupport::Application.config.secret_key_base = TravelSupport::Config.setting(:secret, :key_base)
