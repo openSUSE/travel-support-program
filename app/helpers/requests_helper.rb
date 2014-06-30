12ed635e13b43c8a61b3936a64b857820e72d51a
Ancor Gonzalez Sosa <ancor@suse.de>
2014-06-30 17:57:33 +0200

    Added link to see/create reimbursement in the requests list
    Conflicts:
    	app/views/shipments/index.html.haml

---
+++ app/helpers/requests_helper.rb
@@ -0,0 +1,21 @@
+#
+# Helpers for requests' views
+#
+module RequestsHelper
+
+  # Outputs the link to the reimbursement displayed in the requests list. If
+  # there is no reimbursement, it outputs the button for creating it (if
+  # possible).
+  #
+  # @param [Request] request
+  # @return [String] HTML output
+  def reimbursement_link(request)
+    if request.reimbursement
+      link_to request.reimbursement.label, request_reimbursement_path(request)
+    elsif can?(:create, request.build_reimbursement)
+      link_to t(:create_reimbursement_short), request_reimbursement_path(request), :method => :post, :class => 'btn btn-mini'
+    else
+      "-"
+    end
+  end
+end
---
+++ spec/features/requests_list_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+#require 'ruby-debug'
+
+feature "Requests list", "" do
+  fixtures :all
+
+  scenario "with pending reimbursement" do
+    sign_in_as_user(users(:luke))
+    visit requests_path
+    find("#request-#{requests(:luke_for_party).id} .reimbursement").should have_content "-"
+    find("#request-#{requests(:luke_for_yavin).id} .reimbursement").should have_link "Create"
+    # Click on the "create" button
+    find("#request-#{requests(:luke_for_yavin).id} .reimbursement a").click
+    page.should have_content "reimbursement was successfully created"
+    # And go back to the list
+    visit requests_path
+    find("#request-#{requests(:luke_for_yavin).id} .reimbursement").should have_link requests(:luke_for_yavin).reimbursement.label
+  end
+end
